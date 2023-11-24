e90d36ac7d0571baf873f9e734c65740a22031d1
Kui Hua Wang <98484628+KuiHua-JAC@users.noreply.github.com>
2023-11-24 14:50:24 -0500

    Update AnimatedText.tsx

--- components/AnimatedText.tsx
+++ components/AnimatedText.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 // https://blog.logrocket.com/3-ways-implement-typing-animation-react/
 export default function Typewriter({
