a028c08398c6e95fe4af6a8031121140948231ce
Kui Hua Wang <98484628+KuiHua-JAC@users.noreply.github.com>
2023-11-24 14:59:54 -0500

    update package lock

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import FunFacts from "@/components/funFacts";
-import AboutMe from "@/components/funFacts";
 import PersonalPresentation from "@/components/personalPresentation";
 import ProfessionalCareer from "@/components/professionalCareer";
 import TechnicalSkills from "@/components/technicalSkills";
