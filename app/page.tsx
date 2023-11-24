a9f059165d8b15fd71da82f318799aa98cec965e
Kui Hua Wang <98484628+KuiHua-JAC@users.noreply.github.com>
2023-11-24 01:55:35 -0500

    simplify navlinks code

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div className="text-primary">mainsd</div>;
+  return <div className="text-primary">maiddnsd</div>;
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,18 +8,25 @@ export default function Navbar() {
     active: "text-primary",
     inactive: "text-tertiary hover:text-primary",
   };
+
+  const links = [
+    { href: "/", label: "Home" },
+    { href: "/about-me", label: "About me" },
+    { href: "/portfolio", label: "Portfolio" },
+  ];
+
   return (
-    <nav className="bg-secondary border-gray-200 ">
+    <nav className="bg-secondary border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <span className="self-center text-2xl font-semibold whitespace-nowrap text-primary">
+          <span className="self-center text-3xl font-semibold whitespace-nowrap text-primary font-serif">
             Kui Hua Wang
           </span>
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <button
             type="button"
-            className="text-tertiary bg-secondary hover:bg-primary border border-primary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
+            className="text-primary bg-secondary hover:bg-primary border-2 border-primary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
           >
             Contact me
           </button>
@@ -53,36 +60,18 @@ export default function Navbar() {
           id="navbar-cta"
         >
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 bg-secondary ">
-            <li>
-              <Link
-                href={"/"}
-                className={`${
-                  pathName === "/" ? button.active : button.inactive
-                }`}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                href={"/about-me"}
-                className={`${
-                  pathName === "/about-me" ? button.active : button.inactive
-                }`}
-              >
-                About me
-              </Link>
-            </li>
-            <li>
-              <Link
-                href={"/portfolio"}
-                className={`${
-                  pathName === "/portfolio" ? button.active : button.inactive
-                }`}
-              >
-                Portfolio
-              </Link>
-            </li>
+            {links.map(({ href, label }) => (
+              <li key={href}>
+                <Link
+                  href={href}
+                  className={`${
+                    pathName === href ? button.active : button.inactive
+                  }`}
+                >
+                  {label}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
