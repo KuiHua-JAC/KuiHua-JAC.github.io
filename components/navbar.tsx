5709a01a3a3aa54546e7b1a6d51099ea015e92ee
Kui Hua Wang <98484628+KuiHua-JAC@users.noreply.github.com>
2023-11-26 03:34:04 -0500

    add alerts for non working features

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -24,15 +24,23 @@ export default function Navbar() {
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <button
+            onClick={() => {
+              alert(
+                "Feature to come. On the meantime, my contact information is in the footer."
+              );
+            }}
             type="button"
             className="text-primary bg-secondary hover:bg-primary border-2 hover:text-secondary border-primary font-medium rounded-2xl text-sm px-4 py-2 text-center"
           >
             Contact me
           </button>
           <button
+            onClick={() => {
+              alert("Feature to come. Desktop version is working correctly.");
+            }}
             data-collapse-toggle="navbar-cta"
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-2xl md:hidden hover:bg-tertiary focus:outline-none focus:ring-2 focus:ring-gray-200"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-primary rounded-2xl md:hidden hover:bg-primary hover:text-secondary focus:outline-none focus:ring-2"
             aria-controls="navbar-cta"
             aria-expanded="false"
           >
