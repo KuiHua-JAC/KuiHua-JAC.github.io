60ba9d00ebfb26d053ef0617813306f2e81463b4
Kui Hua Wang <98484628+KuiHua-JAC@users.noreply.github.com>
2023-12-18 17:04:20 -0500

    Update page.tsx

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -5,9 +5,6 @@ import GithubRepos from "@/components/githubRepos";
 export default function Portfolio() {
   return (
     <>
-      <h1 className="text-secondary text-3xl font-bold">
-        Page generated using GitHub REST API
-      </h1>
       <GithubRepos username="KuiHua-JAC" />
     </>
   );
